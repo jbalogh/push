3a64b3056d6a4fda2f119d864d187ee70785aa4a
Jeff Balogh <jbalogh@mozilla.com>
2011-12-14 16:55:37 -0800

    add a nav.notifications.checkPermission API

--- push/static/include.js
+++ push/static/include.js
@@ -7,7 +7,7 @@
   /* Communication frame with jschannel. */
   var frame = document.createElement('iframe');
   frame.id = 'child';
-  frame.src = 'http://push.app/child.html#' + document.location.host;
+  frame.src = 'http://push.app/child.html?x=' + (new Date).getTime() + '#' + document.location.host;
   frame.style.height = '400px';
   frame.style.width = '250px';
   document.body.appendChild(frame);
@@ -21,12 +21,18 @@
   /** navigator.notifications shim */
   navigator.notifications = {};
 
-  navigator.notifications.requestPermission = function(cb) {
+  navigator.notifications.requestPermission = function requestPermission(cb) {
     var site = location.host;
     if (window.confirm('Allow push notifications for ' + site + '?')) {
         channel.call({method: 'requestPermission',
                       success: cb,
                       error: function(e, m){ console.log(e, m) }});
     }
+  };
+
+  navigator.notifications.checkPermission = function checkPermission(cb) {
+    channel.call({method: 'checkPermission',
+                  success: cb,
+                  error: function(e, m){ console.log(e, m); }});
   };
 })();
