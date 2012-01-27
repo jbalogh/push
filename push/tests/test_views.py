848eaace8b10e82cf614767bfd2a6f05b7acaa3a
Jeff Balogh <jbalogh@mozilla.com>
2012-01-27 13:00:58 -0800

    tests for queue_has_token

--- push/tests/test_views.py
+++ push/tests/test_views.py
@@ -15,12 +15,21 @@ def assert_error(code, message, response):
     eq_(message, response[1])
 
 
+def Request(post=None, matchdict=None):
+    request = testing.DummyRequest(post=post)
+    if matchdict:
+        request.matchdict = matchdict
+    if not hasattr(request, 'validated'):
+        request.validated = {}
+    return request
+
+
 class ViewTest(unittest2.TestCase):
 
     def setUp(self):
         self.config = testing.setUp()
 
-        self.request = testing.DummyRequest()
+        self.request = Request()
         self.storage = push.storage.mem.Storage()
         self.queuey = MockQueuey()
         self.request.registry['storage'] = self.storage
@@ -39,61 +48,56 @@ class ViewTest(unittest2.TestCase):
         eq_(response, {'token': mock.sentinel.token})
 
     def test_has_token_and_registration_id(self):
-        request = testing.DummyRequest(post={'token': ''})
+        request = Request(post={'token': ''})
         response = views.has_token_and_registration_id(request)
         assert_error(400, 'Missing required argument: token', response)
 
-        request = testing.DummyRequest(post={'token': 'ok'})
+        request = Request(post={'token': 'ok'})
         response = views.has_token_and_registration_id(request)
         assert_error(400, 'Missing required argument: registration_id',
                      response)
 
-        request = testing.DummyRequest(post={'registration_id': 'ok'})
+        request = Request(post={'registration_id': 'ok'})
         response = views.has_token_and_registration_id(request)
         assert_error(400, 'Missing required argument: token', response)
 
-        request = testing.DummyRequest(post={'token': 'ok',
-                                             'registration_id': ''})
+        request = Request(post={'token': 'ok', 'registration_id': ''})
         response = views.has_token_and_registration_id(request)
         assert_error(400, 'Missing required argument: registration_id',
                      response)
 
-        request = testing.DummyRequest(post={'token': 't',
-                                             'registration_id': 'r'})
+        request = Request(post={'token': 't', 'registration_id': 'r'})
         eq_(None, views.has_token_and_registration_id(request))
 
     def test_add_droid_id(self):
-        request = testing.DummyRequest(post={'token': 't',
-                                             'registration_id': 'r'})
+        request = Request(post={'token': 't', 'registration_id': 'r'})
         eq_(views.add_droid_id(request), {'ok': 'ok'})
 
         eq_(self.storage.get_android_id('t'), 'r')
 
     def test_has_token_and_domain(self):
-        request = testing.DummyRequest(post={'token': ''})
+        request = Request(post={'token': ''})
         response = views.has_token_and_domain(request)
         assert_error(400, 'Missing required argument: token', response)
 
-        request = testing.DummyRequest(post={'token': 'ok'})
+        request = Request(post={'token': 'ok'})
         response = views.has_token_and_domain(request)
         assert_error(400, 'Missing required argument: domain', response)
 
-        request = testing.DummyRequest(post={'domain': 'ok'})
+        request = Request(post={'domain': 'ok'})
         response = views.has_token_and_domain(request)
         assert_error(400, 'Missing required argument: token', response)
 
-        request = testing.DummyRequest(post={'token': 'ok',
-                                             'domain': ''})
+        request = Request(post={'token': 'ok', 'domain': ''})
         response = views.has_token_and_domain(request)
         assert_error(400, 'Missing required argument: domain', response)
 
-        request = testing.DummyRequest(post={'token': 't',
-                                             'domain': 'r'})
+        request = Request(post={'token': 't', 'domain': 'r'})
         eq_(None, views.has_token_and_domain(request))
 
     def test_new_queue(self):
         self.queuey.new_queue = lambda: 'new-queue'
-        request = testing.DummyRequest(post={'token': 't', 'domain': 'x.com'})
+        request = Request(post={'token': 't', 'domain': 'x.com'})
         request.route_url = lambda s, **kw: s.format(**kw)
         response = views.new_queue(request)
         eq_(response, {'queue': '/queue/new-queue/'})
@@ -101,3 +105,10 @@ class ViewTest(unittest2.TestCase):
         assert self.storage.user_owns_queue('t', 'new-queue')
         assert self.storage.domain_owns_queue('x.com', 'new-queue')
         eq_(self.storage.get_user_for_queue('new-queue'), 't')
+
+    def test_queue_has_token(self):
+        request = Request(matchdict={'queue': 'queue'})
+        assert_error(404, 'Not Found', views.queue_has_token(request))
+
+        self.storage.new_queue('queue', 'user', 'domain')
+        eq_(views.queue_has_token(request), None)
