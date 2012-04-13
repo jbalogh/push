f988e2b71edeeda24d2a41e481f9d50b6c3a2118
Jeff Balogh <jbalogh@mozilla.com>
2012-04-13 15:20:12 -0700

    store node status in redis

--- push/storage/redis_.py
+++ push/storage/redis_.py
@@ -8,6 +8,7 @@ class Storage(StorageBase):
     u = 'push:u:1:'  # Users.
     d = 'push:d:1:'  # Domains.
     a = 'push:a:1:'  # Android tokens.
+    n = 'push:n:1:'  # Edge node status.
 
     def __init__(self, **kw):
         self.redis = redis.Redis(**kw)
@@ -38,3 +39,16 @@ class Storage(StorageBase):
 
     def get_android_id(self, user):
         return self.redis.get(self.a + user)
+
+    def set_edge_node_status(self, node, num_connections):
+        self.redis.zadd(self.n, node, num_connections)
+
+    def get_edge_nodes(self, num=None):
+        """Get edge nodes sorted by ascending number of connections."""
+        # If num is None get all of them.
+        if num is None:
+            num = -1
+        return self.redis.zrange(self.n, 0, num)
+
+    def remove_edge_node(self, node):
+        self.redis.zrem(self.n, node)
